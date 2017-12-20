483c55ead7faf73f79f5d1a622b9431817336e23
josh-simar <30680994+josh-simar@users.noreply.github.com>
2017-12-20 15:02:35 -0800

    Update and rename 2-16 StayinFront AppPool.ps1 to New-StayinFrontAppPool.ps1

--- New-StayinFrontAppPool.ps1
+++ New-StayinFrontAppPool.ps1
@@ -1,13 +1,12 @@
-﻿$bDebug = 0
+$bDebug = 0
 $LoadFolder = '\\NVSFTCTRLP01\C$\ASMTouchChecks\PROD\Environment\'
 $LoadServerFile = $LoadFolder + 'WEBServers.txt'
 $Computers       = Get-Content $LoadServerFile
 
-Import-Module WebAdministration
-
 foreach ($computer in $Computers)
 {
     $NewPSSession = New-PSSession -ComputerName $computer
+    Invoke-Command -Session $NewPSSession -ScriptBlock { Import-Module WebAdministration }
     Invoke-Command -Session $NewPSSession -ScriptBlock { Set-ItemProperty -Path IIS:\AppPools\StayinFront -Name managedRuntimeVersion -Value 'v2.0' }
     Invoke-Command -Session $NewPSSession -ScriptBlock { Set-ItemProperty -Path IIS:\AppPools\StayinFront -Name Failure.RapidFailProtection -Value 'False' }
     Invoke-Command -Session $NewPSSession -ScriptBlock { Set-ItemProperty -Path IIS:\AppPools\StayinFront -Name Recycling.PeriodicRestart.time -Value '00:00:00' }
