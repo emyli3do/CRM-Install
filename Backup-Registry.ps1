6b7b22292f8f0bd28be1b6f0ee7b46c4c2ceb40a
josh-simar <30680994+josh-simar@users.noreply.github.com>
2018-01-05 17:13:12 -0800

    Update and rename 2-2 Backup Registry.ps1 to Backup-Registry.ps1

---
+++ Backup-Registry.ps1
@@ -0,0 +1,59 @@
+function Backup-Registry {
+<#
+	.SYNOPSIS
+		Create the StayinFront AppPool with default settings
+	
+	.DESCRIPTION
+		Create the StayinFront AppPool with default settings
+	
+	.PARAMETER ComputerName
+		The target Server(s). Defaults to localhost.
+	
+	.PARAMETER Credential
+		Allows you to login to $ComputerName using alternative credentials.
+    .PARAMETER Path
+		The path to store the file that has the information
+		
+	.PARAMETER WhatIf
+		If this switch is enabled, no actions are performed but informational messages will be displayed that explain what would happen if the command were to run.
+	
+	.PARAMETER Confirm
+		If this switch is enabled, you will be prompted for confirmation before executing any operations that change state.
+	
+	.EXAMPLE
+		New-StayinFrontAppPool -ComputerName Server1 -Path C:\temp\ServicesSttings.csv
+		
+		Creates the StayinFront AppPool with default settings
+	
+	.NOTES
+		Tags: Services
+		
+		Website: N/A
+		Copyright: (C) Josh Simar, josh.simar@advantagesolutions.net
+		License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
+#>
+	[CmdletBinding(SupportsShouldProcess, ConfirmImpact = "High")]
+	param (
+		[parameter(ValueFromPipeline)]$ComputerName = $env:COMPUTERNAME,
+        [string]$BackupFolder
+		[PSCredential]$Credential
+        [string]$RegistryKey
+		#[string]$Path Eventually add logging
+	)
+
+begin
+{
+    If(!($RegistryKey)) {$RegistryKey = "HKLM\SOFTWARE\Wow6432Node\StayinFront"}
+    If (!(Test-Path $BackupFolder)) {New-Item $BackupFolder -directory}
+    
+    ForEach ($computer in $ComputerName)
+    {
+        $NewPSSession = New-PSSession -ComputerName $computer
+        Invoke-Command -Session $NewPSSession -ScriptBlock { 
+        If (!(Test-Path C:\Temp\)) {New-Item C:\Temp -directory}
+        cmd /c "reg export $ReristryKey C:\Temp\Registry.reg"
+        }
+        Remove-PSSession -Session $NewPSSession
+        Copy-Item -Path \\$computer\C$\Temp\Registry.reg -Destination C:\Temp\Registry\$computer.reg
+    }
+}
