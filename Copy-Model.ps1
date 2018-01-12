9063f3b4cff6e1d0e2144275b9917bef9aa25023
josh-simar <30680994+josh-simar@users.noreply.github.com>
2018-01-12 12:37:33 -0800

    Update Copy-Model.ps1

--- Copy-Model.ps1
+++ Copy-Model.ps1
@@ -1,40 +1,40 @@
 function Copy-Model {
 <#
-	.SYNOPSIS
-		Copies the model folder contents to the model folder on the server.
-	
-	.DESCRIPTION
-		Copies the model folder contents to the model folder on the server.
-	
-	.PARAMETER ComputerName
-		The target Server(s).
-	
-	.PARAMETER Credential
-		Allows you to login to $ComputerName using alternative credentials.
-    
+    .SYNOPSIS
+        Copies the model folder contents to the model folder on the server.
+
+    .DESCRIPTION
+        Copies the model folder contents to the model folder on the server.
+
+    .PARAMETER ComputerName
+        The target Server(s).
+
+    .PARAMETER Credential
+        Allows you to login to $ComputerName using alternative credentials.
+
     .PARAMETER ReleasePath
         Path where the release folder is kept.
 
     .PARAMETER PushPath
-        Path where the release folder is kept.
+        Path where we are pushing the release to.
 
-	.PARAMETER WhatIf
-		If this switch is enabled, no actions are performed but informational messages will be displayed that explain what would happen if the command were to run.
-	
-	.PARAMETER Confirm
-		If this switch is enabled, you will be prompted for confirmation before executing any operations that change state.
-	
-	.EXAMPLE
-		$Computers | Copy-Model -ReleasePath "\\asm.lan\dcshare\App\SIF\Prod\Data\!CurrentRelease" -PushPath "C:\Program Files(x86)\StayinFront\CRM\Systems\ASM"
-		
-		Starts IIS on all the servers that are in the $Computers array
-	
-	.NOTES
-		Tags: Web IIS Start
-		
-		Website: N/A
-		Copyright: (C) Josh Simar, josh.simar@advantagesolutions.net
-		License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
+    .PARAMETER WhatIf
+        If this switch is enabled, no actions are performed but informational messages will be displayed that explain what would happen if the command were to run.
+
+    .PARAMETER Confirm
+        If this switch is enabled, you will be prompted for confirmation before executing any operations that change state.
+
+    .EXAMPLE
+        $Computers | Copy-Model -ReleasePath "\\dcshare\Apps\Prod\Data\!CurrentRelease" -PushPath "C:\Program Files(x86)\CRM\Systems\Ours"
+        
+        Copies Model folder to all computers for newest release.
+
+    .NOTES
+        Tags: Model Copy
+        
+        Website: N/A
+        Copyright: (C) Josh Simar, josh.simar@advantagesolutions.net
+        License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
 #>
 	[CmdletBinding(SupportsShouldProcess, ConfirmImpact = "High")]
 	param (
