c60c2d11ff9e6e95e7bdd49187bc8bf1767f4d15
josh-simar <30680994+josh-simar@users.noreply.github.com>
2018-03-19 14:32:21 -0700

    IT WORKS! No touche!

--- functions/Install-CRMLanguage.ps1
+++ functions/Install-CRMLanguage.ps1
@@ -1,53 +1,86 @@
-﻿function Install-CRMLanguage {
+function Install-CRMLanguagePack {
 <#
-	.SYNOPSIS
-		Create the StayinFront AppPool with default settings
-	
-	.DESCRIPTION
-		Create the StayinFront AppPool with default settings
-	
-	.PARAMETER ComputerName
-		The target Server(s). Defaults to localhost.
-	
-	.PARAMETER Credential
-		Allows you to login to $ComputerName using alternative credentials.
-		
-	.PARAMETER Path
-		The path to store the file that has the information
-		
-	.PARAMETER WhatIf
-		If this switch is enabled, no actions are performed but informational messages will be displayed that explain what would happen if the command were to run.
-	
-	.PARAMETER Confirm
-		If this switch is enabled, you will be prompted for confirmation before executing any operations that change state.
-	
-	.EXAMPLE
-		Backup-Registry -ComputerName $Computer -BackupFolder "C:\Temp" -RegistryKey $reg
-		
-		Backs up the registry key of reg on remote computer $Computer To the local folder of C:\Temp
-	
-	.NOTES
-		Tags: Backup Registry
-		
-		Website: N/A
-		Copyright: (C) Josh Simar, josh.simar@advantagesolutions.net
-		License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
+       .SYNOPSIS
+             Installs the CRM Language Pack
+       
+       .DESCRIPTION
+             Installs the CRM Language Pack
+       
+       .PARAMETER ComputerName
+             The target Servers. Defaults to localhost.
+       
+       .PARAMETER Credential
+             Allows you to login to $ComputerName using alternative credentials.
+                    
+       .PARAMETER WhatIf
+             If this switch is enabled, no actions are performed but informational messages will be displayed that explain what would happen if the command were to run.
+       
+       .PARAMETER Confirm
+             If this switch is enabled, you will be prompted for confirmation before executing any operations that change state.
+       
+       .EXAMPLE
+             Get-ServiceSettings -ComputerName Server1 -Path C:\temp\ServicesSttings.csv
+             
+             Adds the local C:\temp\cert.cer to the remote server Server1 in LocalMachine\My (Personal).
+       
+       .NOTES
+             Tags: Services
+             
+             Website: N/A
+             Copyright: (C) Josh Simar, josh.simar@advantagesolutions.net
+             License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
 #>
-	[CmdletBinding(SupportsShouldProcess, ConfirmImpact = "High")]
-	param (
-		[parameter(ValueFromPipeline)][string[]]$ComputerName = $env:COMPUTERNAME
-		#[string]$Path Eventually add logging
-	)
-	begin
+       [CmdletBinding(SupportsShouldProcess, ConfirmImpact = "High")]
+       param (
+             [parameter(ValueFromPipeline)]$ComputerName = $env:COMPUTERNAME,
+             [string]$Path
+       )
+
+process
     {
+        #$bDebug = 0
+        #$LoadFolder = '\\NVSFTCTRLP01\C$\ASMTouchChecks\PROD\Environment\'
+        #$LoadServerFile = $LoadFolder + 'ALLServersNoCitrix.txt'
+        #$Computers = Get-Content $LoadServerFile
+        $sourcefile = "$ReleasePath\StayinFrontCRM-x64 12.3.2.535\StayinFrontCRM-Languages 13.0.0.1738.msi"
 
-    }
-	process
-	{
-        $CRMLanguagePack = New-PSSession -ComputerName $ComputerName        
-        Invoke-Command -Session $CRMLanguagePack -ScriptBlock { IF (Test-Path 'C:\Temp\StayinFrontLanguages.txt') {Remove-Item 'C:\Temp\StayinFrontLanguages.txt' }}
-        Invoke-Command -Session $CRMLanguagePack -ScriptBlock { cmd /c 'msiexec.exe /qn /i "C:\Temp\StayinFrontLanguageInstall\StayinFrontCRM-Languages 13.0.0.1310.msi" /l*vx C:\Temp\StayinFrontLanguages.txt' } -AsJob
+        $ReleasePath = "\\asm.lan\dcshare\App\SIF\Prod\Data\!CurrentRelease"
 
-        Remove-PSSession -Session $CRMLanguagePack
+        $jobscript = {
+            Param($computer)
+            
+             Invoke-Command  -ScriptBlock { cmd /c 'msiexec.exe /qn /i "C:\Temp\StayinFrontLanguageInstall\StayinFrontCRM-Languages 13.0.0.1738.msi" /l*vx C:\Temp\StayinFrontLanguages.txt' }
+        }
+        
+        ForEach ($computer in $ComputerName)
+        {
+            
+            $destinationFolder = "\\$computer\C$\Temp\StayinFrontLanguageInstall"
+
+            if (Test-Path -path $destinationFolder)
+            {
+                Write-Verbose -Message "Removing Old Folder on $computer"
+                if ($pscmdlet.ShouldProcess("$destinationfolder", "Remove Directory")) {Remove-Item $destinationFolder -Recurse}
+            }
+            Write-Verbose -Message "Creating Install Package Folder on $computer"
+            if ($pscmdlet.ShouldProcess("$destinationfolder", "Create Directory")) {New-Item $destinationFolder -ItemType Directory |out-null}
+
+            Write-Verbose -Message "Copying Install Package to $computer"
+            if ($pscmdlet.ShouldProcess("$destinationfolder", "Copy File $sourcefile")) {Copy-Item -Path $sourcefile -Destination $destinationFolder}
+            Write-Verbose -Message "Connecting to $computer"
+            Start-Job -ScriptBlock $jobscript -ArgumentList $computer
+        }
     }
 }
+
+
+
+Best Regards,
+Emylie Do
+Database Engineer
+Advantage Solutions 
+O: +1 949-797-2950 x20950 | M: +1 909-896-8885 
+Emylie.do@advantagesolutions.net|www.advantagesolutions.net
+
+This e-mail message, including any attachments, is for the sole use of the intended recipient(s) and may contain information that is confidential and protected by law from unauthorized disclosure. Any unauthorized review, use, disclosure or distribution is prohibited. If you are not the intended recipient, please contact the sender by reply e-mail and destroy all copies of the original message.
+
