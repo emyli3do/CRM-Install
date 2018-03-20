21b5845a16581dc93ce5bb4b7ab089fa4d024417
josh-simar <30680994+josh-simar@users.noreply.github.com>
2018-03-20 14:29:04 -0700

    Update Reboot-Server.ps1

--- functions/Reboot-Server.ps1
+++ functions/Reboot-Server.ps1
@@ -1,7 +1,55 @@
-ForEach ($computer in $computers)
-{
-    If ($computer -ne $env:COMPUTERNAME)
+function Reboot-Server {
+<#
+	.SYNOPSIS
+		Create the StayinFront AppPool with default settings
+	
+	.DESCRIPTION
+		Create the StayinFront AppPool with default settings
+	
+	.PARAMETER ComputerName
+		The target Server(s). Defaults to localhost.
+	
+	.PARAMETER Credential
+		Allows you to login to $ComputerName using alternative credentials.
+		
+	.PARAMETER Path
+		The path to store the file that has the information
+		
+	.PARAMETER WhatIf
+		If this switch is enabled, no actions are performed but informational messages will be displayed that explain what would happen if the command were to run.
+	
+	.PARAMETER Confirm
+		If this switch is enabled, you will be prompted for confirmation before executing any operations that change state.
+	
+	.EXAMPLE
+		Reboot-Server -ComputerName $Computer
+		
+		Backs up the registry key of reg on remote computer $Computer To the local folder of C:\Temp
+	
+	.NOTES
+		Tags: Reboot
+		
+		Website: N/A
+		Copyright: (C) Josh Simar, josh.simar@advantagesolutions.net
+		License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
+#>
+	[CmdletBinding(SupportsShouldProcess, ConfirmImpact = "High")]
+	param (
+		[parameter(ValueFromPipeline)]
+        [string[]]$ComputerName = $env:COMPUTERNAME
+	)
+	begin
     {
-        Restart-Computer -ComputerName $computer -Force
+
+    }
+	process
+	{
+        ForEach ($computer in $computers)
+        {
+            If ($computer -ne $env:COMPUTERNAME)
+            {
+                Restart-Computer -ComputerName $computer -Force
+            }
+        }
     }
 }
