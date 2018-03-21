3a24603476d6936f36748625e90addd10f446a89
josh-simar <30680994+josh-simar@users.noreply.github.com>
2018-03-20 17:08:25 -0700

    Update Backup-Registry.ps1

--- functions/Backup-Registry.ps1
+++ functions/Backup-Registry.ps1
@@ -1,23 +1,23 @@
 function Backup-Registry {
 <#
-	.SYNOPSIS
-		Create the StayinFront AppPool with default settings
+    .SYNOPSIS
+        Create the StayinFront AppPool with default settings
+
+    .DESCRIPTION
+        Create the StayinFront AppPool with default settings
+
+    .PARAMETER ComputerName
+        The target Server(s). Defaults to localhost.
+
+    .PARAMETER Credential
+        Allows you to login to $ComputerName using alternative credentials.
 	
-	.DESCRIPTION
-		Create the StayinFront AppPool with default settings
-	
-	.PARAMETER ComputerName
-		The target Server(s). Defaults to localhost.
-	
-	.PARAMETER Credential
-		Allows you to login to $ComputerName using alternative credentials.
+    .PARAMETER Path
+        The path to store the file that has the information
 		
-	.PARAMETER Path
-		The path to store the file that has the information
-		
-	.PARAMETER WhatIf
-		If this switch is enabled, no actions are performed but informational messages will be displayed that explain what would happen if the command were to run.
-	
+    .PARAMETER WhatIf
+        If this switch is enabled, no actions are performed but informational messages will be displayed that explain what would happen if the command were to run.
+
 	.PARAMETER Confirm
 		If this switch is enabled, you will be prompted for confirmation before executing any operations that change state.
 	
