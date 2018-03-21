c9a1390ba694fdf54fcd9b68a91992ff6bcd0af8
josh-simar <30680994+josh-simar@users.noreply.github.com>
2018-03-20 17:30:26 -0700

    Update Run-DataLoads.ps1

--- functions/Run-DataLoads.ps1
+++ functions/Run-DataLoads.ps1
@@ -1,12 +1,60 @@
-﻿$bDebug = 0
-$LoadFolder = '\\NVSFTCTRLP01\C$\ASMTouchChecks\PROD\Environment\'
-$LoadServerFile = $LoadFolder + 'ALLServersNoCitrix.txt'
-$Computers       = Get-Content $LoadServerFile
+function Run-DataLoads {
+<#
+    .SYNOPSIS
+        Copies the CRM folder contents to the CRM folder on the server.
+    .DESCRIPTION
+        Copies the CRM folder contents to the CRM folder on the server.
+    .PARAMETER ComputerName
+        The target Server(s).
+    .PARAMETER Credential
+        Allows you to login to $ComputerName using alternative credentials.
+    .PARAMETER ReleasePath
+        Path where the release folder is kept.
+    .PARAMETER PushPath
+        Path where we are pushing the release to.
+    .PARAMETER WhatIf
+        If this switch is enabled, no actions are performed but informational messages will be displayed that explain what would happen if the command were to run.
+    .PARAMETER Confirm
+        If this switch is enabled, you will be prompted for confirmation before executing any operations that change state.
+    .EXAMPLE
+        $Computers | Copy-CRM -ReleasePath "\\dcshare\Apps\Prod\Data\!CurrentRelease" -PushPath "C:\Program Files(x86)\CRM\Systems\Ours"
+        
+        Copies CRM folder to all computers for newest release.
+    .NOTES
+        Tags: CRM Copy
+        
+        Website: N/A
+        Copyright: (C) Josh Simar, josh.simar@advantagesolutions.net
+        License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
+#>
+	[CmdletBinding(SupportsShouldProcess, ConfirmImpact = "High")]
+param
+    (
+    [parameter(Mandatory=$true)]
+    [string]$ReleasePath
+	)
+process
+    {
+        $destinationFolder = "C:\Temp\DataLoads"
+        
+        if (Test-Path -path $destinationFolder)
+        {
+            Write-Verbose -Message "Removing Old Folder on $computer"
+            if ($pscmdlet.ShouldProcess("$destinationfolder", "Remove Directory")) {Remove-Item $destinationFolder -Recurse}
+        }
+        Write-Verbose -Message "Creating DataLoads Package Folder on $computer"
+        If ($pscmdlet.ShouldProcess("$destinationfolder", "Create Directory")) {New-Item $destinationFolder -ItemType Directory |out-null}
 
-New-Item "C:\Temp\DataLoadsScripts" -ItemType Directory
-Copy-Item -Container "\\asm.lan\dcshare\App\SIF\Prod\Data\!CurrentRelease\Data-Loads" -Destination "C:\Temp\DataLoadsScripts\" -Recurse
-
-cmd /c 'C:\Temp\DataLoadsScripts\Data-Loads\Script\Run Script.cmd'
-cmd /c 'C:\Temp\DataLoadsScripts\Data-Loads\Script 2\Run Script.cmd'
-cmd /c 'C:\Temp\DataLoadsScripts\Data-Loads\Script 3\Run Script.cmd'
-cmd /c 'C:\Temp\DataLoadsScripts\Data-Loads\Workflow\UpdateSelectedWorkflow.cmd'
+        Write-Verbose -Message "Copying DataLoads Files to $computer"
+        If ($pscmdlet.ShouldProcess("Item: $ReleasePath Destination: $destinationFolder\", "Copy DataLoads"))
+        {
+            Copy-Item -Path "$ReleasePath\Data-Loads\" -Destination $destinationFolder\ -Recurse -Force
+        }
+        foreach ($runfile in (Get-ChildItem $destinationFolder\ -Filter ".cmd"))
+        {
+            $runfile = $runfile.FullPath
+            $cmd = "cmd /c ""$runfile"" /y"
+            cmd /c $cmd
+        }
+    }
+}
