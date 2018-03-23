090cf35cd0372e89a3767f10b5a3e9296288104c
josh-simar <30680994+josh-simar@users.noreply.github.com>
2018-03-23 14:44:53 -0700

    Update Remove-CRM.ps1

--- functions/Remove-CRM.ps1
+++ functions/Remove-CRM.ps1
@@ -1,55 +1,48 @@
 function Remove-CRM {
 <#
-	.SYNOPSIS
-		Create the StayinFront AppPool with default settings
-	
-	.DESCRIPTION
-		Create the StayinFront AppPool with default settings
-	
-	.PARAMETER ComputerName
-		The target Server(s). Defaults to localhost.
-	
-	.PARAMETER Credential
-		Allows you to login to $ComputerName using alternative credentials.
-		
-	.PARAMETER Path
-		The path to store the file that has the information
-		
-	.PARAMETER WhatIf
-		If this switch is enabled, no actions are performed but informational messages will be displayed that explain what would happen if the command were to run.
-	
-	.PARAMETER Confirm
-		If this switch is enabled, you will be prompted for confirmation before executing any operations that change state.
-	
-	.EXAMPLE
-		Backup-Registry -ComputerName $Computer -BackupFolder "C:\Temp" -RegistryKey $reg
-		
-		Backs up the registry key of reg on remote computer $Computer To the local folder of C:\Temp
-	
-	.NOTES
-		Tags: Backup Registry
-		
-		Website: N/A
-		Copyright: (C) Josh Simar, josh.simar@advantagesolutions.net
-		License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
+    .SYNOPSIS
+        Create the StayinFront AppPool with default settings
+    
+    .DESCRIPTION
+        Create the StayinFront AppPool with default settings
+    
+    .PARAMETER ComputerName
+        The target Server(s). Defaults to localhost.
+    
+    .PARAMETER Credential
+        Allows you to login to $ComputerName using alternative credentials.
+        
+    .PARAMETER Path
+        The path to store the file that has the information
+        
+    .PARAMETER WhatIf
+        If this switch is enabled, no actions are performed but informational messages will be displayed that explain what would happen if the command were to run.
+    
+    .PARAMETER Confirm
+        If this switch is enabled, you will be prompted for confirmation before executing any operations that change state.
+    
+    .EXAMPLE
+        Backup-Registry -ComputerName $Computer -BackupFolder "C:\Temp" -RegistryKey $reg
+        
+        Backs up the registry key of reg on remote computer $Computer To the local folder of C:\Temp
+    
+    .NOTES
+        Tags: Backup Registry
+        
+        Website: N/A
+        Copyright: (C) Josh Simar, josh.simar@advantagesolutions.net
+        License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
 #>
-	[CmdletBinding(SupportsShouldProcess, ConfirmImpact = "High")]
-	param (
-		[parameter(ValueFromPipeline)][string[]]$ComputerName = $env:COMPUTERNAME
-		#[string]$Path Eventually add logging
-	)
-	begin
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = "High")]
+    param (
+        [parameter(ValueFromPipeline)][string[]]$ComputerName = $env:COMPUTERNAME
+    )
+    begin
     {
 
     }
-	process
-	{
-        $CRMLanguagePack = New-PSSession -ComputerName $_        
-
-        Invoke-Command -Session $CRMLanguagePack -ScriptBlock { $app = Get-WmiObject -Class Win32_Product | Where-Object {$_.Name -match "StayinFront CRM"} }
-        Invoke-Command -Session $CRMLanguagePack -ScriptBlock { $cmd = "msiexec.exe /x " + $app.IdentifyingNumber + " /qn /L*vx C:\Temp\StayinFrontCRM-x64Uninstall.txt" }
-        Invoke-Command -Session $CRMLanguagePack -ScriptBlock { cmd /c $cmd } -AsJob
-
-        Remove-PSSession -Session $CRMLanguagePack
+    process
+    {
+        Invoke-WmiMethod -Path "Win32_Product.Name='StayinFront CRM'" -Computer $_ -Name Uninstall
     }
 }
