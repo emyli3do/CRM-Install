2f2590f6ab5a8183cd6cff7c227a0754af18962a
josh-simar <30680994+josh-simar@users.noreply.github.com>
2018-03-26 12:59:54 -0700

    Update Copy-APK.ps1

--- functions/Copy-APK.ps1
+++ functions/Copy-APK.ps1
@@ -33,8 +33,9 @@ function Copy-APK {
         Copyright: (C) Josh Simar, josh.simar@advantagesolutions.net
         License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
 #>
-	[CmdletBinding(SupportsShouldProcess, ConfirmImpact = "High")]
-	param (
+	[CmdletBinding(SupportsShouldProcess, ConfirmImpact = "Medium")]
+param
+    (
 		[parameter(ValueFromPipeline,Mandatory=$true)]
 		[string[]]$ComputerName,
 		[parameter(Mandatory=$true)]
