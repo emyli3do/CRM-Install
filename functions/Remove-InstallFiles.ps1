0f4442b1f85525df7bc1350b4565d554aad60dfb
josh-simar <30680994+josh-simar@users.noreply.github.com>
2018-03-26 18:02:15 -0700

    Create Remove-InstallFiles.ps1

---
+++ functions/Remove-InstallFiles.ps1
@@ -0,0 +1,40 @@
+function Remove-InstallFiles {
+<#
+    .SYNOPSIS
+        Removes no longer needed files from install
+    .DESCRIPTION
+        Removes no longer needed files from install
+    .PARAMETER ComputerName
+        The target Server(s).    
+    .PARAMETER WhatIf
+        If this switch is enabled, no actions are performed but informational messages will be displayed that explain what would happen if the command were to run.
+    .PARAMETER Confirm
+        If this switch is enabled, you will be prompted for confirmation before executing any operations that change state.
+        
+    .EXAMPLE
+        $Computers | Remove-AER
+        
+        Copies Model folder to all computers for newest release.
+        
+    .NOTES
+        Tags: APK Copy
+        
+        Website: N/A
+        Copyright: (C) Josh Simar, josh.simar@advantagesolutions.net
+        License: GNU GPL v3 https://opensource.org/licenses/GPL-3.0
+#>
+	[CmdletBinding(SupportsShouldProcess, ConfirmImpact = "Medium")]
+	param (
+		[parameter(ValueFromPipeline,Mandatory=$true)]
+		[string[]]$ComputerName,
+	)
+process
+    {
+        $computer = $_
+        
+        Remove-Item -Path \\$computer\C$\StayinFrontInstall | Out-Null
+        Remove-Item -Path \\$computer\C$\StayinFrontLanguageInstall | Out-Null
+        Unregister-ScheduledTask -TaskName "Install StayinFront CRM Languages" -Confirm:$false
+        Unregister-ScheduledTask -TaskName "Install StayinFront CRM" -Confirm:$false
+    }
+}
